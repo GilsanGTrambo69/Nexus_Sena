8c24deadb59a48a317573a201cb5bc6186a16495
GilsanGTrambo69 <gilsanforero13@gmail.com>
2025-01-26 14:53:04 -0500

    gil12

---
+++ assets/php/sidebar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>sidebar</title>
+</head>
+<body>
+        <aside class="sidebar" id="sidebar">
+            <nav>
+                <ul class="nav-menu">
+                    <li class="nav-item">
+                        <a href="#"><button class="button-sidebar"><img src="./assets/img/home.png" alt="">Inicio</button></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#"><button class="button-sidebar"><img src="./assets/img/Amigos.png" alt="">Amigos</button></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#"><button class="button-sidebar"><img src="./assets/img/encuestas.png" alt="">Encuestas</button></a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#"><button class="button-sidebar"><img src="./assets/img/mensajes.png" alt="">Mensajes</button></a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+</body>
+</html>
